d661a69bdf2827a5a3792e5094372969bf899e5d
Sarah Young <hi.sarah.young@gmail.com>
2018-10-20 23:21:59 +0200

    Commiting work for the evening.

--- my-app/src/components/dog_breed_list.js
+++ my-app/src/components/dog_breed_list.js
@@ -1,1 +1,217 @@
-//Container for dog breeds
+import React from 'react';
+
+const BreedListBlock = (props) => {
+  const dogBreedButtons = props.dogs.message
+}
+
+// Create function that takes in object from App props
+// Returns button list UI
+
+class
+let dogBreeds =
+{
+    "status": "success",
+    "message": {
+        "affenpinscher": [],
+        "african": [],
+        "airedale": [],
+        "akita": [],
+        "appenzeller": [],
+        "basenji": [],
+        "beagle": [],
+        "bluetick": [],
+        "borzoi": [],
+        "bouvier": [],
+        "boxer": [],
+        "brabancon": [],
+        "briard": [],
+        "bulldog": [
+            "boston",
+            "french"
+        ],
+        "bullterrier": [
+            "staffordshire"
+        ],
+        "cairn": [],
+        "cattledog": [
+            "australian"
+        ],
+        "chihuahua": [],
+        "chow": [],
+        "clumber": [],
+        "cockapoo": [],
+        "collie": [
+            "border"
+        ],
+        "coonhound": [],
+        "corgi": [
+            "cardigan"
+        ],
+        "cotondetulear": [],
+        "dachshund": [],
+        "dalmatian": [],
+        "dane": [
+            "great"
+        ],
+        "deerhound": [
+            "scottish"
+        ],
+        "dhole": [],
+        "dingo": [],
+        "doberman": [],
+        "elkhound": [
+            "norwegian"
+        ],
+        "entlebucher": [],
+        "eskimo": [],
+        "germanshepherd": [],
+        "greyhound": [
+            "italian"
+        ],
+        "groenendael": [],
+        "hound": [
+            "afghan",
+            "basset",
+            "blood",
+            "english",
+            "ibizan",
+            "walker"
+        ],
+        "husky": [],
+        "keeshond": [],
+        "kelpie": [],
+        "komondor": [],
+        "kuvasz": [],
+        "labrador": [],
+        "leonberg": [],
+        "lhasa": [],
+        "malamute": [],
+        "malinois": [],
+        "maltese": [],
+        "mastiff": [
+            "bull",
+            "tibetan"
+        ],
+        "mexicanhairless": [],
+        "mix": [],
+        "mountain": [
+            "bernese",
+            "swiss"
+        ],
+        "newfoundland": [],
+        "otterhound": [],
+        "papillon": [],
+        "pekinese": [],
+        "pembroke": [],
+        "pinscher": [
+            "miniature"
+        ],
+        "pointer": [
+            "german"
+        ],
+        "pomeranian": [],
+        "poodle": [
+            "miniature",
+            "standard",
+            "toy"
+        ],
+        "pug": [],
+        "puggle": [],
+        "pyrenees": [],
+        "redbone": [],
+        "retriever": [
+            "chesapeake",
+            "curly",
+            "flatcoated",
+            "golden"
+        ],
+        "ridgeback": [
+            "rhodesian"
+        ],
+        "rottweiler": [],
+        "saluki": [],
+        "samoyed": [],
+        "schipperke": [],
+        "schnauzer": [
+            "giant",
+            "miniature"
+        ],
+        "setter": [
+            "english",
+            "gordon",
+            "irish"
+        ],
+        "sheepdog": [
+            "english",
+            "shetland"
+        ],
+        "shiba": [],
+        "shihtzu": [],
+        "spaniel": [
+            "blenheim",
+            "brittany",
+            "cocker",
+            "irish",
+            "japanese",
+            "sussex",
+            "welsh"
+        ],
+        "springer": [
+            "english"
+        ],
+        "stbernard": [],
+        "terrier": [
+            "american",
+            "australian",
+            "bedlington",
+            "border",
+            "dandie",
+            "fox",
+            "irish",
+            "kerryblue",
+            "lakeland",
+            "norfolk",
+            "norwich",
+            "patterdale",
+            "russell",
+            "scottish",
+            "sealyham",
+            "silky",
+            "tibetan",
+            "toy",
+            "westhighland",
+            "wheaten",
+            "yorkshire"
+        ],
+        "vizsla": [],
+        "weimaraner": [],
+        "whippet": [],
+        "wolfhound": [
+            "irish"
+        ]
+    }
+}
+
+let breeds = dogBreeds.message;
+// Loop through object & create list of keys (1)
+let breedList = Object.keys(breeds);
+
+// create new Array()
+let dogBreedList = [];
+
+// Iterate through hashmap
+// > Create for loop
+let i; let j;
+for (i = 0; i < breedList.length; i++) {
+  // iterate through keys of Object
+  let dogType = breeds[breedList[i]];
+  if (dogType.length === 0){
+  dogBreedList.push(breedList[i]);
+} else {
+  for (j = 0; j < dogType.length; j++) {
+    dogBreedList.push(dogType[j] + " " + breedList[i]);
+  }
+
+  }
+}
+console.log(dogBreedList);
