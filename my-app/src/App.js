bd8a5168c4a4404cd0a611ec9ccb9b9716032473
Sarah Young <hi.sarah.young@gmail.com>
2018-10-20 20:23:01 +0200

    Adding files

---
+++ my-app/src/App.js
@@ -0,0 +1,290 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dogs: [],
+      selectedBreed: null,
+      greeting: 'Select a dog breed.',
+      isLoading: true
+    }; //state
+    console.log("Hi from state");
+  } //constructor
+
+//beginning old thing
+//   getDogs() {
+//     axios
+//     // Dog CEO API call for breed list
+//     .get("https://dog.ceo/api/breeds/list/all")
+//     .then(response =>
+//       response.data.message(dog => ({
+//         breedlist: `${dog.message}`
+//       }))
+//     )
+//     .then(dogs => {
+//           this.setState({
+//             dogs,
+//             isLoading: false,
+//           });
+//         })
+//     .catch(error => this.setState({ error, isLoading: false }));
+//     console.log("In getDogs");
+//   } //getDogs
+//
+// componentDidMount() {
+//   this.getDogs();
+//   console.log("In componentDidMount");
+// }
+//end old thing
+// beginning new thing
+
+componentDidMount() {
+    axios.get(`https://dog.ceo/api/breeds/list/all`)
+      .then(res => {
+        const dogs = res.data;
+        this.setState({ dogs });
+      })
+  }
+//end new thing
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>They Are All Good Dogs</h1>
+        </header>
+        <p>
+        { this.state.selectedBreed }
+        </p>
+        <p>
+        { this.state.greeting }
+        </p>
+      </div>
+      // Insert doggo image in <p>
+    );
+  } //render
+} // Component
+
+let dogBreeds =
+{
+    "status": "success",
+    "message": {
+        "affenpinscher": [],
+        "african": [],
+        "airedale": [],
+        "akita": [],
+        "appenzeller": [],
+        "basenji": [],
+        "beagle": [],
+        "bluetick": [],
+        "borzoi": [],
+        "bouvier": [],
+        "boxer": [],
+        "brabancon": [],
+        "briard": [],
+        "bulldog": [
+            "boston",
+            "french"
+        ],
+        "bullterrier": [
+            "staffordshire"
+        ],
+        "cairn": [],
+        "cattledog": [
+            "australian"
+        ],
+        "chihuahua": [],
+        "chow": [],
+        "clumber": [],
+        "cockapoo": [],
+        "collie": [
+            "border"
+        ],
+        "coonhound": [],
+        "corgi": [
+            "cardigan"
+        ],
+        "cotondetulear": [],
+        "dachshund": [],
+        "dalmatian": [],
+        "dane": [
+            "great"
+        ],
+        "deerhound": [
+            "scottish"
+        ],
+        "dhole": [],
+        "dingo": [],
+        "doberman": [],
+        "elkhound": [
+            "norwegian"
+        ],
+        "entlebucher": [],
+        "eskimo": [],
+        "germanshepherd": [],
+        "greyhound": [
+            "italian"
+        ],
+        "groenendael": [],
+        "hound": [
+            "afghan",
+            "basset",
+            "blood",
+            "english",
+            "ibizan",
+            "walker"
+        ],
+        "husky": [],
+        "keeshond": [],
+        "kelpie": [],
+        "komondor": [],
+        "kuvasz": [],
+        "labrador": [],
+        "leonberg": [],
+        "lhasa": [],
+        "malamute": [],
+        "malinois": [],
+        "maltese": [],
+        "mastiff": [
+            "bull",
+            "tibetan"
+        ],
+        "mexicanhairless": [],
+        "mix": [],
+        "mountain": [
+            "bernese",
+            "swiss"
+        ],
+        "newfoundland": [],
+        "otterhound": [],
+        "papillon": [],
+        "pekinese": [],
+        "pembroke": [],
+        "pinscher": [
+            "miniature"
+        ],
+        "pointer": [
+            "german"
+        ],
+        "pomeranian": [],
+        "poodle": [
+            "miniature",
+            "standard",
+            "toy"
+        ],
+        "pug": [],
+        "puggle": [],
+        "pyrenees": [],
+        "redbone": [],
+        "retriever": [
+            "chesapeake",
+            "curly",
+            "flatcoated",
+            "golden"
+        ],
+        "ridgeback": [
+            "rhodesian"
+        ],
+        "rottweiler": [],
+        "saluki": [],
+        "samoyed": [],
+        "schipperke": [],
+        "schnauzer": [
+            "giant",
+            "miniature"
+        ],
+        "setter": [
+            "english",
+            "gordon",
+            "irish"
+        ],
+        "sheepdog": [
+            "english",
+            "shetland"
+        ],
+        "shiba": [],
+        "shihtzu": [],
+        "spaniel": [
+            "blenheim",
+            "brittany",
+            "cocker",
+            "irish",
+            "japanese",
+            "sussex",
+            "welsh"
+        ],
+        "springer": [
+            "english"
+        ],
+        "stbernard": [],
+        "terrier": [
+            "american",
+            "australian",
+            "bedlington",
+            "border",
+            "dandie",
+            "fox",
+            "irish",
+            "kerryblue",
+            "lakeland",
+            "norfolk",
+            "norwich",
+            "patterdale",
+            "russell",
+            "scottish",
+            "sealyham",
+            "silky",
+            "tibetan",
+            "toy",
+            "westhighland",
+            "wheaten",
+            "yorkshire"
+        ],
+        "vizsla": [],
+        "weimaraner": [],
+        "whippet": [],
+        "wolfhound": [
+            "irish"
+        ]
+    }
+}
+
+let breeds = dogBreeds.message;
+// Loop through object & create list of keys (1)
+let breedList = Object.keys(breeds);
+
+// create new Array()
+let dogBreedList = [];
+
+// Iterate through hashmap
+// > Create for loop
+let i; let j;
+for (i = 0; i < breedList.length; i++) {
+  // iterate through keys of Object
+  let dogType = breeds[breedList[i]];
+  if (dogType.length === 0){
+  dogBreedList.push(breedList[i]);
+} else {
+  for (j = 0; j < dogType.length; j++) {
+    dogBreedList.push(dogType[j] + " " + breedList[i]);
+  }
+
+  }
+}
+console.log(dogBreedList);
+
+  //using index of corresponding array
+
+
+
+
+
+
+
+
+
+export default App;
---
+++ my-app/src/components/dog_breed_list.js
@@ -0,0 +1,1 @@
+//Container for dog breeds
