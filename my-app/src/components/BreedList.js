558ee73b272ec65f81e8a78d868a729ac03d0990
Sarah Young <hi.sarah.young@gmail.com>
2018-10-23 11:56:29 +0200

    Cleaned up some code, looking into Warning: Breed:  is not a prop. Trying to access it will result in  being returned. If you need to access the same value within the child component, you should pass it as a different prop. (https://fb.me/react-special-props).

--- my-app/src/components/BreedList.js
+++ my-app/src/components/BreedList.js
@@ -6,19 +6,17 @@ import Breed from './Breed';
 class BreedList extends React.Component {
 
   render() {
-    const imageterm = this.props.imageterm;
-    const breedlistobject = this.props.dogs;
+    const breedlistobj = this.props.dogs;
     const title = "Dog Breed List";
     const rows = [];
-    const getDog = this.props.getDog;
-    let keylist = Object.keys(breedlistobject);
+    let keylist = Object.keys(breedlistobj);
     console.log(keylist);
     let i; let j;
     for (i = 0; i < keylist.length; i++) {
-      if (breedlistobject[keylist[i]].length === 0) {
-    rows.push(<Breed breed={keylist[i]} key={"breed-list-" + keylist[i]} />)
-    } else { for (j = 0; j < breedlistobject[keylist[i]].length; j++) {
-    rows.push(<Breed breed={breedlistobject[keylist[i]][j] + " " + keylist[i]} id={keylist[i]+"-"+breedlistobject[keylist[i]][j]}/>)
+      if (breedlistobj[keylist[i]].length === 0) {
+    rows.push(<Breed breed={keylist[i]} key={keylist[i]} />)
+    } else { for (j = 0; j < breedlistobj[keylist[i]].length; j++) {
+    rows.push(<Breed breed={breedlistobj[keylist[i]][j] + " " + keylist[i]} key={keylist[i]+"-"+breedlistobj[keylist[i]][j]}/>)
       }
     }
   }
