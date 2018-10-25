b5958ceccc8bb9fea4b457f5a2817ece95baf0a7
Sarah Young <hi.sarah.young@gmail.com>
2018-10-25 21:25:11 +0200

    Tweaking responsive grid.

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,9 @@
-import changeDoggo from './App';
 
+
+import { changeDoggo }  from './App ';
 
 
 it('changeDoggo works as intended', () => {
   const testCD = changeDoggo("terrier-american", "american terrier");
-  console.log(testCD);
+  console.log('foo');
 });
