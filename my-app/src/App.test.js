04e0606338b46726e94628184fd0773dda5fe132
Sarah Young <hi.sarah.young@gmail.com>
2018-10-28 16:38:02 +0100

    Updated package.json to prep for testing.

--- my-app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-
-
-import { changeDoggo }  from './App ';
-
-
-it('changeDoggo works as intended', () => {
-  const testCD = changeDoggo("terrier-american", "american terrier");
-  console.log('foo');
-});
