c5cc7d18ea5699576a593ca965af02f1ae006ee8
Sarah Young <hi.sarah.young@gmail.com>
2018-11-07 20:48:12 +0100

    Adding setupTests.js

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+import  Enzyme, { shallow } from 'enzyme';
+
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+  //disableLifecycleMethods: true,
+ });
